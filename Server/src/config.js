ef0beb973d414a32a8a533d5b3be9f2622b34e0b
Mosaicpan <mosaicpan@tencent.com>
2019-10-13 22:24:35 +0800

    配置文件读取

--- Server/src/config.js
+++ Server/src/config.js
@@ -1,7 +1,23 @@
 import fs from "fs"
-
-async function readConfig(){
-
+function readFile(...params){
+    return new Promise((resolve,reject)=>{
+        fs.readFile(...params,(err,data)=>{
+            if(err){
+                reject(err)
+            }else{
+                resolve(data)
+            }
+        })
+    })
 }
 
-export default readConfig
+export async function readConfig(filePath = "./conf.json"){
+    try{
+        let conf = await readFile(filePath)
+        conf = conf.toString();
+        conf = JSON.parse(conf);
+        return conf;
+    }catch(e){
+        console.error(`Read Config Failed ${filePath}`,e.message)
+    }
+}
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,20 +1,50 @@
-import app from "./app"
+import fs from "fs"
 import http from "http"
-import config from "./config"
+import https from "https"
+import app from "./app"
+import {readConfig} from "./config"
 
-let server;
+let servers = [];
 let currentApp = app;
 
+async function init(){
+    // 初始化配置文件，开启服务器
+    const config = await readConfig();
+    // 检查 http server
+    if(config.http && config.http.enable){
+        try{
+            let httpServer = http.createServer(app);
+            httpServer.listen(config.http.port || 80);
+            servers.push(httpServer);
+            console.log(`|- HTTP Server Started`)
+        }catch(e){
+            console.error(`|- HTTP Server Init Failed`,e.message);
+        }
+    }
+    // 检查 https server
+    if(config.https && config.https.enable){
+        try{
+            let httpsServer = https.createServer({
+                key: fs.readFileSync(config.https.key),
+                cert: fs.readFileSync(config.https.crt)
+            },app)
+            httpsServer.listen(config.httpsServer.port || 443);
+            server.push(httpsServer)
+            console.log(`|- HTTPS Server Started`)
+        }catch(e){
+            console.error(`|- HTTPS Server Init Failed`,e.message);
+        }
+    }
+}
 
-
-server = http.createServer(app)
-server.listen(80);
-
+init(); //服务器初始化
 
 if (module.hot) {
     module.hot.accept('./app', () => {
-        server.removeListener('request', currentApp)
-        server.on('request', app)
+        servers.forEach(server=>{
+            server.removeListener('request', currentApp)
+            server.on('request', app)
+        })
         currentApp = app
     })
 }
